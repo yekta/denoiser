c08871994268e0c3b654253b69269a9bcbf58498
yekta <ngyekta@gmail.com>
2023-06-12 02:50:57 +0300

    Bump dependencies

--- setup.py
+++ setup.py
@@ -11,31 +11,32 @@ from pathlib import Path
 
 from setuptools import setup
 
-NAME = 'denoiser'
+NAME = "denoiser"
 DESCRIPTION = (
-    'Speech enhancement in the waveform domain.'
-    ' Supports offline and streaming evaluation.'
-    ' Implementation for https://arxiv.org/abs/2006.12847.'
-    ' For training, please directly clone the github repository.')
+    "Speech enhancement in the waveform domain."
+    " Supports offline and streaming evaluation."
+    " Implementation for https://arxiv.org/abs/2006.12847."
+    " For training, please directly clone the github repository."
+)
 
-URL = 'https://github.com/facebookresearch/denoiser'
-EMAIL = 'alexandre.defossez@gmail.com'
-AUTHOR = 'Alexandre Défossez'
-REQUIRES_PYTHON = '>=3.7.0'
+URL = "https://github.com/facebookresearch/denoiser"
+EMAIL = "alexandre.defossez@gmail.com"
+AUTHOR = "Alexandre Défossez"
+REQUIRES_PYTHON = ">=3.7.0"
 VERSION = "0.1.5"
 
 HERE = Path(__file__).parent
 
 REQUIRED = [
-    'julius',
-    'hydra_core<1.0',
-    'hydra_colorlog<1.0',
-    'numpy>=1.19',
-    'pystoi>=0.3.3',
-    'six',
-    'sounddevice>=0.4',
-    'torch>=1.5',
-    'torchaudio>=0.5',
+    "julius",
+    "hydra_core",
+    "hydra_colorlog",
+    "numpy>=1.19",
+    "pystoi>=0.3.3",
+    "six",
+    "sounddevice>=0.4",
+    "torch>=1.5",
+    "torchaudio>=0.5",
 ]
 
 REQUIRED_LINKS = [
@@ -43,8 +44,8 @@ REQUIRED_LINKS = [
 ]
 
 try:
-    with open(HERE / "README.md", encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with open(HERE / "README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -53,20 +54,20 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=['denoiser'],
+    packages=["denoiser"],
     install_requires=REQUIRED,
     dependency_link=REQUIRED_LINKS,
     include_package_data=True,
-    license='Creative Commons Attribution-NonCommercial 4.0 International',
+    license="Creative Commons Attribution-NonCommercial 4.0 International",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Topic :: Multimedia :: Sound/Audio :: Speech',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
